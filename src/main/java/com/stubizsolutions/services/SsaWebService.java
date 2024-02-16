defe16d93c641e732ceb1b5463707c295b53c436
balaji46102 <balajiin99@gmail.com>
2024-02-16 12:38:49 +0530

    change made in ssa webservice

--- src/main/java/com/stubizsolutions/services/SsaWebService.java
+++ src/main/java/com/stubizsolutions/services/SsaWebService.java
@@ -3,6 +3,8 @@ package com.stubizsolutions.services;
 import com.stubizsolutions.bindings.SsaWebRequest;
 import com.stubizsolutions.bindings.SsaWebResponse;
 
+               //service class1
+
 public interface SsaWebService 
 {
 	public SsaWebResponse getCitizenInfo(SsaWebRequest request);
